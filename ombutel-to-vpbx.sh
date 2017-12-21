3ba9043a34c96e02e673d6953a67264895a19a13
Jose Rivera <ing.joserivera26@gmail.com>
2017-12-21 10:47:06 -0600

    Remove old ssh welcome
    Remove the all ssh welcome script and execute the vpbx ssh welcome script

--- ombutel-to-vpbx.sh
+++ ombutel-to-vpbx.sh
@@ -11,5 +11,9 @@ rm -rf /var/lib/asterisk/sounds/fr_CA_f_June
 # Reinstall the latest version of asterisk sounds
 yum reinstall asterisk-sounds-* -y
 
+# Remove the old ssh welcome
+rm -rf /etc/profile.d/obtwelcome.sh
+
 # All Done
 echo Welcome to VitalPBX!
+/bin/bash /etc/profile.d/vitalwelcome.sh
